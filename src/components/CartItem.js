818b84a7fd35924d3e0a9f4523b15b7375e26e92
dereje1212 <aragawdereje876@gmail.com>
2022-12-09 03:12:50 +0300

    second commit

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,35 +1,27 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import "./Cart.css";
-import { cartActions } from "./../store/cartSlice";
-const CartItem = ({ name, quantity, total, price, id }) => {
-  const dispatch = useDispatch();
-  const removeHandler = () => {
-    dispatch(cartActions.removeFromCart(id));
-  };
-  const addHandler = () => {
-    dispatch(
-      cartActions.addToCart({
-        id,
-        name,
-        price,
-      })
-    );
-  };
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { cartActions } from '../store/cart-clice'
+import './Cart.css'
+const CartItem =({name, quantity, total, price, id}) => {
+ const dispatch =useDispatch()
+  const incrementCartItem =()=>{
+ dispatch(cartActions.addToCart({
+  name,id,price
+ }))
+  }
+  const decrementCartItems=()=>{
+     dispatch(cartActions.removeFromCart(id))
+  }
   return (
-    <div className="cartItem">
-      <h2> {name}</h2>
-      <p>${price} /-</p>
-      <p>x{quantity}</p>
-      <article>Total ${total}</article>
-      <button className="cart-actions" onClick={removeHandler}>
-        -
-      </button>
-      <button className="cart-actions" onClick={addHandler}>
-        +
-      </button>
+    <div className='cartItem'>
+       <h2>{name}</h2>
+       <p>${price}</p>
+       <p>x {quantity}</p>
+       <article>Total ${total}</article>
+       <button onClick={decrementCartItems} className='cart-actions'>-</button>
+       <button onClick={incrementCartItem} className='cart-actions' >+</button>
     </div>
-  );
-};
+  )
+}
 
 export default CartItem;
